03a46a2b669cec86785af39436115d4a8dced579
Anton Veldanov <anton.veldanov@gmail.com>
2019-10-09 19:26:03 -0700

    algo

--- playground/10/10stringSearch.js
+++ playground/10/10stringSearch.js
@@ -1,15 +1,48 @@
-const stringSearch = (str) => {
+// const stringSearch = (str, check) => {
+//   let j = 0;
+//   let match = 0
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = 0; j < check.length; j++) {
+//       if (str[i] !== check[j]) {
+//         break;
+//       } else {
+//         match++;
+//         continue;
+//       }
 
-  for (let char of str) {
+//     }
 
-    console.log(char);
+
+//   }
+
+//   return match;
+
+// }
+
+const stringSearch = (str, check) => {
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+
+    for (var j = 0; j < check.length; j++) {
+      console.log(check[j], str[[i + j]]);
+      if (check[j] !== str[i + j]) {
+        console.log("BREAK");
+
+        break;
+
+      }
+      if (j === check.length - 1) {
+        count++;
+      }
+
+    }
 
   }
-
-
-
+  return count;
 }
 
 
 
-console.log(stringSearch("weewdomgwwwdc"));
+
+
+console.log(stringSearch("abcde", "cd"));
