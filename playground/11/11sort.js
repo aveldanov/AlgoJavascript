4237690b3faab196fa1a532b2fc90c480c9a311a
Anton Veldanov <anton.veldanov@gmail.com>
2019-10-12 19:29:21 -0700

    algo

--- playground/11/11sort.js
+++ playground/11/11sort.js
@@ -1,10 +1,48 @@
-const sort = () => {
+// const sortArr = (arr) => {
+
+//   return arr.sort((a, b) => {
+//     return a - b
+//   })
 
 
+// }
+
+
+// const sortArr = (arr) => {
+//   let temp = null;
+
+//   let newArr = [];
+//   for (let i = 0; i <= arr.length - 1; i++) {
+//     if (arr[i] > arr[i + 1]) {
+//       temp = arr[i]; //5
+//       arr[i] = arr[i + 1] //3
+//       arr[i + 1] = temp;
+//     } else if (i === arr.length - 1) {
+//       newArr.push(arr[i]);
+//       // arr.pop();
+//       i = 0;
+//     }
+//   }
+//   return newArr;
+
+// }
+
+
+
+
+
+
+const sortArr = (arr) => {
+
+  for (let i = arr.length; i = 0; i--) {
+
+    console.log(i);
+
+
+  }
 
 
 }
 
 
-
-console.log(sort([4, 66, 7, 3, 1, 3]));
+console.log(sortArr([5, 3, 4, 1, 2]));
