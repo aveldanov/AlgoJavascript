95bed1e9ed7c1d2f4da248de1950b3de6de6a1da
Anton Veldanov <anton.veldanov@gmail.com>
2019-10-17 23:26:19 -0700

    algo

---
+++ playground/13/13insert.js
@@ -0,0 +1,24 @@
+const insert = (arr) => {
+
+  for (let i = 1; i < arr.length; i++) {
+    var currentVal = arr[i];
+
+
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      console.log(i, j);
+      console.log("currentVal", currentVal);
+      console.log("arr[j]", arr[j]);
+
+      arr[j + 1] = arr[j]
+      console.log(arr);
+
+
+    }
+    arr[j + 1] = currentVal;
+
+
+  }
+  return arr;
+
+}
+console.log(insert([2, 1, 9, 76, 4]));
