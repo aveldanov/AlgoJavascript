7daf3f5f881c82064d52e54f763156050998b002
Anton Veldanov <anton.veldanov@gmail.com>
2019-10-22 22:58:08 -0700

    algo

--- playground/15/15merge.js
+++ playground/15/15merge.js
@@ -1,33 +1,38 @@
+// const merge = (arr1, arr2) => {
+
+
+//   var arr = [];
+
+//   var minimal = Math.min(arr1.length, arr2.length);
+//   i = 0;
+//   j = 0;
+//   while (i < minimal || j < minimal) {
+
+//     if (arr1[i] < arr2[j]) {
+
+//       arr.push(arr1[i])
+//       i++;
+//     } else {
+//       arr.push(arr2[j]);
+//       j++;
+//     }
+
+
+//   }
+
+//   arr.push()
+
+
+
+//   return arr;
+
+// }
+
 const merge = (arr1, arr2) => {
+  let results = [];
 
-
-  var arr = [];
-
-  var minimal = Math.min(arr1.length, arr2.length);
-  i = 0;
-  j = 0;
-  while (i < minimal || j < minimal) {
-
-    if (arr1[i] < arr2[j]) {
-
-      arr.push(arr1[i])
-      i++;
-    } else {
-      arr.push(arr2[j]);
-      j++;
-    }
-
-
-  }
-
-  arr.push()
-
-
-
-  return arr;
-
+  return results;
 }
-
 
 
 console.log(merge([1, 10, 50, 59, 78, 110, 120], [2, 14, 99, 100, 103]));
